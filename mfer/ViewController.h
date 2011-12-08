f7b17eafee2c5100ba3ed5723358112f4ce77bc3
ChenYu <crazyming411@gmail.com>
2011-12-08 20:48:43 +0800

    Initial Commit

---
+++ mfer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  mfer
+//
+//  Created by Chen-Yu Hsu on 12/8/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ mferTests/mferTests.m
@@ -0,0 +1,32 @@
+//
+//  mferTests.m
+//  mferTests
+//
+//  Created by Chen-Yu Hsu on 12/8/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "mferTests.h"
+
+@implementation mferTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in mferTests");
+}
+
+@end
