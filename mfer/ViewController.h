58e1171810380426774c155754edaf892d7fc849
ChenYu <crazyming411@gmail.com>
2011-12-08 22:25:50 +0800

    Half

--- mfer/ViewController.h
+++ mfer/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+{
+    UIImageView *ii;
+    UIImageView *slot;
+}
 @end
--- mfer/ViewController.m
+++ mfer/ViewController.m
@@ -8,6 +8,14 @@
 
 #import "ViewController.h"
 
+@interface ViewController()
+-(void)dispatchFirstTouchAtPoint:(CGPoint)touchPoint forEvent:(UIEvent *)event;
+-(void)dispatchTouchEvent:(UIView *)theView toPosition:(CGPoint)position;
+-(void)dispatchTouchEndEvent:(UIView *)theView toPosition:(CGPoint)position;
+-(void)animateView:(UIView *)theView toPosition:(CGPoint) thePosition;
+-(void)animateFirstTouchAtPoint:(CGPoint)touchPoint forView:(UIImageView *)theView;
+@end
+
 @implementation ViewController
 
 - (void)didReceiveMemoryWarning
@@ -22,7 +30,92 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    CGRect r=CGRectMake(arc4random()%300, arc4random()%400, 50, 50);
+    UIImage *im=[UIImage imageNamed:@"slot.png"];
+    slot=[[UIImageView alloc] initWithFrame:r];
+    slot.image=im;
+    slot.center=CGPointMake(160, 240);
+    [self.view addSubview:slot];
+    
+    im=[UIImage imageNamed:@"code.png"];
+    ii=[[UIImageView alloc] initWithFrame:r];
+    ii.image=im; 
+    [self.view addSubview:ii];
+    
 }
+
+-(void)dispatchFirstTouchAtPoint:(CGPoint)touchPoint forEvent:(UIEvent *)event
+{
+	if (CGRectContainsPoint([ii frame], touchPoint)) {
+		[self animateFirstTouchAtPoint:touchPoint forView:ii];
+	}
+}
+
+-(void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    for (UITouch *touch in touches) {
+		[self dispatchFirstTouchAtPoint:[touch locationInView:self.view] forEvent:nil];
+	}
+    
+}
+
+-(void)dispatchTouchEvent:(UIView *)theView toPosition:(CGPoint)position
+{
+	if (CGRectContainsPoint([ii frame], position)) {
+		ii.center = position;
+    
+    }
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    for (UITouch *touch in touches) {
+		[self dispatchTouchEvent:[touch view] toPosition:[touch locationInView:self.view]];
+	}
+}
+
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	for (UITouch *touch in touches) {
+		[self dispatchTouchEndEvent:[touch view] toPosition:[touch locationInView:self.view]];
+	}
+    
+    CGFloat xDis=(slot.center.x-ii.center.x);
+    CGFloat yDis=(slot.center.y-ii.center.y);
+    CGFloat distance=sqrt((xDis * xDis) + (yDis * yDis));
+    
+    if(distance<25 && distance>0) ii.center=slot.center;
+}
+
+-(void)animateView:(UIView *)theView toPosition:(CGPoint)thePosition
+{
+	[UIView beginAnimations:nil context:NULL];
+	[UIView setAnimationDuration:0.15];
+	
+	theView.center = thePosition;
+	
+	theView.transform = CGAffineTransformIdentity;
+	[UIView commitAnimations];	
+}
+
+-(void)dispatchTouchEndEvent:(UIView *)theView toPosition:(CGPoint)position
+{   
+	if (CGRectContainsPoint([ii frame], position)) {
+		[self animateView:ii toPosition: position];
+	}
+}
+
+-(void)animateFirstTouchAtPoint:(CGPoint)touchPoint forView:(UIImageView *)theView 
+{
+	[UIView beginAnimations:nil context:nil];
+	[UIView setAnimationDuration:0.15];
+	theView.transform = CGAffineTransformMakeScale(1.1, 1.1);
+	[UIView commitAnimations];
+}
+
+
+ 
+
 
 - (void)viewDidUnload
 {
